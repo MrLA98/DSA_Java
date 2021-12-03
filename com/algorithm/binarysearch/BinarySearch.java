70eb2270c37d62b9fc524f3339b9d24f7ee64ab0
MrLA98 <luh4869@163.com>
2021-12-03 11:15:02 +0800

    二分查找递归和非递归

--- com/algorithm/binarysearch/BinarySearch.java
+++ com/algorithm/binarysearch/BinarySearch.java
@@ -1,5 +1,41 @@
 package com.algorithm.binarysearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
-    
+    public static void main(String[] args) {
+        int[] arr = {2,5,0,3,7,12,6,5,9,22,13,43,52,14};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(bSearchRecur(arr, 0, arr.length-1, 0));
+        System.out.println(bSearch(arr, 0));
+    }
+
+    public static int bSearchRecur(int[] arr, int L, int R, int target) {
+        if (L > R)
+            return -1;
+        int mid = (L + R) >> 1;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            return bSearchRecur(arr, L, mid - 1, target);
+        } else {
+            return bSearchRecur(arr, mid + 1, R, target);
+        }
+    }
+
+    public static int bSearch(int[] arr, int target) {
+        int L = 0, R = arr.length - 1;
+        while (L <= R) {
+            int mid = (L + R) >> 1;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return -1;
+    }
 }

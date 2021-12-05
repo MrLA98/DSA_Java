f50df2deb75dc6be1a8c2bf7f268904454724d06
MrLA98 <luh4869@163.com>
2021-12-05 10:03:55 +0800

    分治 -- 汉诺塔

---
+++ com/algorithm/divideconquer/HanoiTower.java
@@ -0,0 +1,22 @@
+package com.algorithm.divideconquer;
+
+public class HanoiTower {
+    public static void main(String[] args) {
+        hanoiMove(5, "A", "C", "B");
+    }
+
+    public static void hanoiMove(int n, String start, String end, String help){
+        // 只有一个盘，直接移动到end
+        if(n == 1){
+            System.out.println("disk[1]: " + start + "->" + end);
+            return;
+        }
+        // 上面n-1个盘移动到help
+        hanoiMove(n-1, start, help, end);
+        // 最下面一个移动到end
+        System.out.println("disk["+n+"]: " + start + "->" + end);
+        // 刚刚到help上的n-1个移动到end上
+        hanoiMove(n-1, help, end, start);
+    }
+}
+
